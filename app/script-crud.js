29dc256a4bf5ecd5adfd4a5091f31e3eca4b1da9
israel <israelwendellc@gmail.com>
2023-10-06 20:30:24 -0300

    adicionando novas tarefas

--- app/script-crud.js
+++ app/script-crud.js
@@ -1,5 +1,11 @@
 const taskListContainer =document.querySelector ('.app__section-task-list')
 
+const formTask = document.querySelector ('.app__form-add-task')
+const formLabel = document.querySelector ('.app__form-label')
+const toggleFormTaskBtn = document.querySelector ('.app__button--add-task')
+
+
+
 let tarefas = [
     {
         descricao: 'Tarefa concluída',
@@ -12,14 +18,14 @@ let tarefas = [
 ]
 
 
-const taskIconSvg = `<svg class="app_section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
-fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="12" cy="12" r="12" 
-    fill="#FFF" />
-<path
-    d "M9 16.1719119.5938 5.57812121 6.9843819 18.9844L3.42188 13.4062L4.82812 12L19 16.17192"
-    fill="#01080E" 
-/svg>`
+const taskIconSvg = `<svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
+    fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="12" cy="12" r="12" fill="#FFF" />
+    <path
+        d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
+        fill="#01080E" />
+</svg>
+`
 
 function createTask (tarefa) {
     const li = document.createElement ('li')
@@ -42,4 +48,9 @@ function createTask (tarefa) {
 tarefas.forEach ((tarefa)=> {
     const taskIcon = createTask (tarefa)
     taskListContainer.appendChild (taskIcon)
+})
+
+toggleFormTaskBtn.addEventListener ('click', () => {
+    formLabel.textContent = 'Adicionando tarefa'
+    formTask.classList.toggle('hidden')
 })
